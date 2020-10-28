edb8e6aef6e9a662b186f9093c191aa12990d18e
garammaru68 <63288078+garammaru68@users.noreply.github.com>
2020-10-28 16:01:56 +0900

    Window

--- Study/SCoreLib/SStd.h
+++ Study/SCoreLib/SStd.h
@@ -1,1 +1,50 @@
 #pragma once
+#include <Windows.h>
+#include <tchar.h>
+#include <string>
+#include <vector>
+#include <list>
+#include <map>
+#include <algorithm>
+#pragma comment( lib, "Winmm.lib" )
+#pragma comment( lib, "SCoreLib.lib" )
+#pragma comment( lib, "fmod_vc.lib" )
+#pragma comment( lib, "Msimg32.lib" )
+
+using namespace std;
+
+struct SKeyMap
+{
+	bool	m_bFront;
+	bool	m_bLeft;
+	bool	m_bRight;
+	bool	m_bBack;
+	bool	m_bAttack;
+	bool	m_bJump;
+	bool	m_bHangul;
+	bool	m_bCapsLock;
+	bool	m_bNumLock;
+};
+struct SPoint
+{
+	float x;
+	float y;
+};
+extern SKeyMap	g_KeyMap;
+extern float	g_fGameTimer;
+extern float	g_fSecondPerFrame;
+extern HWND		g_hWnd;
+extern HINSTANCE	g_hInstance;
+extern HDC		g_hScreenDC;
+extern HDC		g_hOffScreenDC;
+extern RECT		g_rtClient;
+template<class S>
+class SSingleton
+{
+public:
+	static S& GetInstance()
+	{
+		static S theSingle;
+		return theSingle;
+	}
+};
---
+++ Study/SCoreLib/SWindow.cpp
@@ -0,0 +1,83 @@
+#include "SWindow.h"
+
+HINSTANCE	g_hInstance;
+HWND		g_hWnd = 0;
+RECT		g_rtClient;
+LRESULT CALLBACK WndProc(
+	HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
+bool SWindow::MsgProcess()
+{
+	while (PeekMessage(&m_msg, NULL, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&m_msg);
+		DispatchMessage(&m_msg);
+		if (m_msg.message == WM_QUIT)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+bool SWindow::SetWindow(HINSTANCE hInstance)
+{
+	g_hInstance = hInstance;
+	m_hInstance = hInstance;
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpfnWndProc = WndProc;
+	wc.hInstance = m_hInstance;
+	wc.lpszClassName = L"KGCAWINDOW";
+	if (RegisterClassEx(&wc) == 0)
+	{
+		return false;
+	}
+
+	RECT rt{ 0, 0, 800, 600 };
+	AdjustWindowRect(&rt, WS_OVERLAPPEDWINDOW, FALSE);
+	m_hWnd = CreateWindowEx(
+		WS_EX_APPWINDOW,
+		L"KGCAWINDOw",
+		L"MyGame2",
+		WS_OVERLAPPEDWINDOW,
+		0, 0,
+		rt.right - rt.left,
+		rt.bottom - rt.top,
+		nullptr, nullptr,
+		m_hInstance, nullptr);
+
+	if (m_hWnd == NULL)
+	{
+		return false;
+	}
+	g_hWnd = m_hWnd;
+	GetClientRect(m_hWnd, &m_rtClient);
+	g_rtClient = m_rtClient;
+	ShowWindow(m_hWnd, SW_SHOW);
+	return true;
+}
+
+SWindow::SWindow()
+{
+	ZeroMemory(&m_msg, sizeof(MSG));
+}
+SWindow::~SWindow()
+{
+
+}
