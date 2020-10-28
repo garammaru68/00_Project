c1184bc70fb3662e150ec2410b24e190592de17a
garammaru68 <63288078+garammaru68@users.noreply.github.com>
2020-10-28 15:04:26 +0900

    초기 커밋.

---
+++ CodeSignal/Example.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+bool checkPalindrome(std::string inputString);
+//bool almostIncreasingSequence(std::vector<int> sequence);
+bool almostIncreasingSequence(std::vector<int> sequence);
---
+++ SCoreLib/SCore.cpp
@@ -0,0 +1,1 @@
+#include "SCore.h"
